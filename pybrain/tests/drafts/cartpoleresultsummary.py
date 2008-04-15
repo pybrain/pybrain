0c2be441e8d5ab9ab05624f6d4677d4f54b66cd7
Tom Schaul <tom@idsia.ch>
2008-04-15 12:51:23 +0000

    updated scripts for cartpole experiments

--- pybrain/tests/drafts/cartpoleresultsummary.py
+++ pybrain/tests/drafts/cartpoleresultsummary.py
@@ -10,10 +10,10 @@ filedir = '../temp/cartpole/'
 allfiles = os.listdir(filedir)
 alldata = {}
 for f in allfiles:
-    if f[-7:] == '.pickle':
-        runid = f[-12:-7]
-        runtype = f[:-14]
-        allfits = array(map(lambda x:x[1], pickleReadDict('../temp/cartpole/'+f[:-7])))
+    if f[-12:] == 'small.pickle':
+        runid = f[-17:-12]
+        runtype = f[:-19]
+        allfits = pickleReadDict('../temp/cartpole/'+f[:-7])
         if runtype not in alldata:
             alldata[runtype] = []
         alldata[runtype].append(allfits)
@@ -21,18 +21,22 @@ for f in allfiles:
 for runtype, res in alldata.items():
     print 'Experiment type:', runtype
     print 'Number of runs:', len(res)
+    successes = filter(lambda x: len(x) > 0 and max(x) >= 50000, res)
+    failures = filter(lambda x: len(x) > 0 and max(x) < 50000, res)
+    print 'Successful runs:', len(successes), '(', int(len(successes)/float(len(res))*1000)/10.0, '%)'
+    if len(successes) > 0:
+        print 'Average number of episodes until success (not considering failures):',  sum(map(len, successes))/float(len(successes))
+    if len(failures) > 0:
+        print 'Average fitness (among failures):',  sum(map(max, failures))/float(len(failures))
+    print
     print 'Number of episodes for all runs', map(len, res)
-    
-    successes = filter(lambda x: len(x) > 0 and max(x) >= 100000, res)
-    print 'Successful runs:', len(successes)
     if len(successes) > 0:
         print 'Number of episodes for successful runs', map(len, successes)
-        print 'Average number of episodes until success (not considering failures):',  sum(map(len, successes))/float(len(successes))
     failures = filter(lambda x: len(x) > 0 and max(x) < 100000, res)
     if len(failures) > 0:
         print '(best fitness, nb of episodes) for failed runs:', zip(map(max, failures),map(len, failures))
-        print 'Average fitness (among failures):',  sum(map(max, failures))/float(len(failures))
-    print '\n\n'
+    print '-'*70
+    print
     
             
             
