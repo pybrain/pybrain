8a5edf05926d33b4782fe6cd67db1ede783313e7
Thomas Rückstieß <ruecksti@in.tum.de>
2008-05-22 05:50:31 +0000

    first try with GP for statedependent agent. not working yet...

---
+++ pybrain/rl/agents/statedependentgp.py
@@ -0,0 +1,67 @@
+__author__ = 'Thomas Rueckstiess, ruecksti@in.tum.de'
+
+from scipy import ravel
+from learning import LearningAgent
+from policygradient import PolicyGradientAgent
+from pybrain.structure import StateDependentLayer, IdentityConnection
+from pybrain import buildNetwork
+from pylab import show, ion
+from pybrain.auxiliary import GaussianProcess
+
+class StateDependentAgent(PolicyGradientAgent):
+    """ StateDependentAgent is a learning agent, that adds a GaussianLayer to its module and stores its
+        deterministic inputs (mu) in the dataset.
+    """
+    
+    def __init__(self, module, learner = None):
+        ion()
+        LearningAgent.__init__(self, module, learner)
+        
+        # exploration module (linear flat network)
+        self.explorationmodule = buildNetwork(self.indim, self.outdim, bias=False)
+        
+        # state dependent exploration layer
+        self.explorationlayer = StateDependentLayer(self.outdim, self.explorationmodule, 'explore')
+                
+        # add exploration layer to top of network through identity connection
+        out = self.module.outmodules.pop()
+        self.module.addOutputModule(self.explorationlayer)
+        self.module.addConnection(IdentityConnection(out, self.module['explore'], self.module))
+        self.module.sortModules()
+        
+        # tell learner the new module
+        self.learner.setModule(self.module)
+        
+        # add the log likelihood (loglh) to the dataset and link it to the others
+        self.history.addField('loglh', self.module.paramdim)
+        self.history.link.append('loglh')
+        self.loglh = None
+        
+        # if this flag is set to True, random weights are drawn after each reward,
+        # effectively acting like the vanilla policy gradient alg.
+        self.actaspg = False
+
+        # gaussian process
+        self.gp = GaussianProcess(1, -5, 5, 0.1)
+
+    def newEpisode(self):
+        params = ravel(self.explorationlayer.module.params)
+        target = ravel(sum(self.history.getSequence(self.history.getNumSequences()-1)[2]) / 500)
+                
+        LearningAgent.newEpisode(self)
+        self.explorationlayer.drawRandomWeights()
+
+        print "params: %f, target: %f" % (params, target)
+        if target != 0.0:
+            self.gp.addSample(params, target)
+            self.gp.plotCurves()
+
+    def getAction(self):
+        self.explorationlayer.setState(self.lastobs)
+        action = PolicyGradientAgent.getAction(self)
+        return action
+        
+    def giveReward(self, r):
+        PolicyGradientAgent.giveReward(self, r)
+        if self.actaspg:
+            self.explorationlayer.drawRandomWeights()
