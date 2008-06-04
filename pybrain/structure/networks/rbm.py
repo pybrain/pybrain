92207787a7088960ee9c8e63c020af11d36b6760
Justin S Bayer <bayer.justin@googlemail.com>
2008-06-04 12:01:30 +0000

    - cosmetic changes to the rbms

--- pybrain/structure/networks/rbm.py
+++ pybrain/structure/networks/rbm.py
@@ -9,40 +9,43 @@ __author__ = ('Christian Osendorfer, osendorf@in.tum.de;'
 from pybrain.structure.modules import BiasUnit, SigmoidLayer, LinearLayer
 from pybrain.structure.connections import FullConnection
 from pybrain.structure.networks import Network
+from pybrain.structure.modules.module import Module
+
 
 class Rbm(Network):
 
     def __init__(self, visibledim, hiddendim):
         super(Rbm, self).__init__()
+        Module.__init__(self, visibledim, hiddendim)
         self.visibledim = visibledim
         self.hiddendim = hiddendim
+        
         bias = BiasUnit()
+        vl = self.visibleLayer = LinearLayer(visibledim)
+        hl = self.hidddenLayer = SigmoidLayer(hiddendim)
+        fc = self.fullConnection = FullConnection(vl, hl)
+        bc = self.biasConnection = FullConnection(bias, hl)
         
-        self.visibleLayer = LinearLayer(visibledim)
-        self.hidddenLayer = SigmoidLayer(hiddendim)
-        self.fullConnection = FullConnection(self.visibleLayer, self.hidddenLayer, self.connNames.next())
-        self.biasConnection = FullConnection(bias, self.hidddenLayer)
+        self.addInputModule(vl)
+        self.addOutputModule(hl)
+        self.addConnection(fc)
+        self.addConnection(bc)
         
-        self.addInputModule(self.visibleLayer)
-        self.addOutputModule(self.hiddenLayer)
-        self.addModule(bias)
-        self.addConnection(self.fullConnection)
-        self.addConnection(self.biasConnection)
         self.sortModules()
 
     def _getWeights(self):
-        return self.connections[0].params
+        return self.fullConnection.params
 
     def _setWeights(self, val):
-        self.connections[0].params[:] = val
+        self.fullConnection.params[:] = val
     
     weights = property(_getWeights, _setWeights)
     
     def _getBiasWeights(self):
-        return self.connections[1].params
+        return self.biasConnection.params
         
     def _setBiasWeights(self, value):
-        self.connections[1].paramse = value
+        self.biasConnection.params = value
         
     biasWeights = property(_getBiasWeights, _setBiasWeights)
 
--- pybrain/unsupervised/trainers/rbmtrainer.py
+++ pybrain/unsupervised/trainers/rbmtrainer.py
@@ -6,15 +6,18 @@ __author__ = ('Christian Osendorfer, osendorf@in.tum.de;'
               'Justin S Bayer, bayerj@in.tum.de')
 
 
+from scipy import array, dot, random, outer
+
+from pybrain.structure.networks.rbm import Rbm
 from pybrain.supervised.trainers import Trainer
-import numpy
 
 
 class RbmTrainer(Trainer):
 
     def __init__(self, rbm):
         self.rbm = rbm
-        self._data = None    
+        self._invRbm = Rbm(self.rbm.hiddendim, self.rbm.visibledim)
+        self._invRbm.weights = self.rbm.weights.T
     
     def _getData(self): 
         return self._data
@@ -27,8 +30,6 @@ class RbmTrainer(Trainer):
     data = property(_getData, _setData)
     
     def trainOnDataset(self, dataset, learningrate=.1, batchsize=50):
-        self._invRbm = Rbm(self.rbm.hiddendim, self.rbm.visibledim)
-        self._invRbm.weights = self.rbm.weights.T
         for rows in dataset.randomBatches('sample', batchsize):
             uWeights, uHidBias, uVisBias = self.calcUpdateByRows(rows)
             # Normalization and learning rate factor
@@ -39,41 +40,49 @@ class RbmTrainer(Trainer):
     
     def calcUpdateByRow(self, row):
         """Return a 3-tuple consiting of updates for (weightmatrix, 
-        hidden bias weights, visual bias weights)."""
+        hidden bias weights, visible bias weights)."""
         # Probabilities of hidden layer in positive phase
         poshp = self.rbm.activate(row)
         # Stochastic Binary Units: Bernoulli sampling
-        sampled = positive > numpy.random.rand(1, self.rbm.hiddendim)
-        sampled = numpy.astype(sampled, numpy.int32)
+        sampled = poshp > random.rand(1, self.rbm.hiddendim)
+        sampled = sampled.astype('int32')
         # "Reconstruction" of the input
         recons = self._invRbm.activate(sampled)
         # Probabilities of hidden layer in negative phase
         neghp = self.rbm.activate(recons)
         # Gradient update for weights
-        pos = numpy.dot(row.T, poshp)       # Fraction from the positive phase
-        neg = numpy.dot(recons.T, neghp)    # Fraction from the first CD step
+        pos = outer(row, poshp)       # Fraction from the positive phase
+        neg = outer(recons, neghp)    # Fraction from the first CD step
         return pos - neg, poshp - neghp, row - recons
         
     def calcUpdateByRows(self, rows):
         """Return a 3-tuple constisting of update for (weightmatrix, 
-        hidden bias weights, visual bias weights)."""
+        hidden bias weights, visible bias weights)."""
         # Probabilities of hidden layer in positive phase
         poshp = array([self.rbm.activate(row) for row in rows])
         # Stochastic Binary Units: Bernoulli sampling
-        sample_shape = self.rows.shape[0], self.rbm.hiddendim
-        sampled = positive > numpy.random.random(shape)
-        sampled.dtype = 'int32'
+        sample_shape = rows.shape[0], self.rbm.hiddendim
+        sampled = poshp > random.random(sample_shape)
+        sampled = sampled.astype('int32')
         # "Reconstruction" of the input
         recons = array([self._invRbm.activate(sample) for sample in sampled])
         # Probabilities of hidden layer in negative phase
         neghp = array([self.rbm.activate(row) for row in recons])
         # Gradient update for weights
-        pos = numpy.dot(row.T, poshp)    # Fraction from the positive phase
-        neg = numpy.dot(recons.T, neghp) # Fraction from the first CD step
+        pos = outer(row, poshp.T)    # Fraction from the positive phase
+        neg = outer(recons, neghp.T) # Fraction from the first CD step
         # Gradient update for hidden bias 
         poshact = poshp.sum(axis=0)
         neghact = neghp.sum(axis=0)
         # Gradient update for visual bias
         posvact = rows.sum(axis=0)
         negvact = recons.sum(axis=0)
-        return pos - neg, posvact - negvact, poshact - neghact
+
+        try:
+            weightupdate = pos - neg
+        except: 
+            print pos.shape, neg.shape, 0
+            return 0, 0, 0
+        hiddenbiasupdate = posvact - negvact 
+        visiblebiasupdate = poshact - neghact
+        return weightupdate, hiddenbiasupdate, visiblebiasupdate
