653c9950739a2323068bde6e77d4a3571d7cb749
Thomas Rückstieß <ruecksti@in.tum.de>
2008-07-21 05:52:00 +0000

    use statedependent layer without GPs

--- pybrain/rl/agents/__init__.py
+++ pybrain/rl/agents/__init__.py
@@ -2,5 +2,5 @@ from agent import Agent
 from flatnetwork import FlatNetworkAgent
 from learning import LearningAgent
 from policygradient import PolicyGradientAgent
-from statedependentgp import StateDependentAgent
+from statedependent import StateDependentAgent
 from finitedifference import FiniteDifferenceAgent
