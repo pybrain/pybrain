e5856c7c1cc8a3dd79286a0178974569ae24782a
Martin Felder <felder@in.tum.de>
2008-09-30 11:14:18 +0000

    fixed XOR example

--- examples/backprop/backpropxor.py
+++ examples/backprop/backpropxor.py
@@ -7,7 +7,7 @@ from pybrain.supervised import BackpropTrainer
 
 def testTraining():
     d = SequentialXORDataSet()
-    n = buildNetwork(d.indim, 4, d.outdim)
+    n = buildNetwork(d.indim, 4, d.outdim, recurrent=True)
     t = BackpropTrainer(n, learningrate = 0.01, momentum = 0.99, verbose = True)
     t.trainOnDataset(d, 1000)
     t.testOnData(verbose= True)
