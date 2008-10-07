8bfe82dd60641272baaf2893cd3eba317c63823b
Sun Yi <yi@idsia.ch>
2008-10-07 18:42:07 +0000

    Updated by SUN Yi

--- examples/rbm/simple.py
+++ examples/rbm/simple.py
@@ -6,16 +6,19 @@ __version__ = '$Id$'
 
 
 from pybrain.structure.networks.rbm import buildRbm
-from pybrain.unsupervised.trainers.rbmtrainer import RbmGibbsTrainer
+from pybrain.unsupervised.trainers.rbmtrainer import RbmGibbsTrainerConfig, RbmGibbsTrainer
 from pybrain.datasets import SupervisedDataSet
 
 
-ds = SupervisedDataSet(6, 1)
+ds = SupervisedDataSet(6, 2)
 ds.addSample([0, 1] * 3, [0])
 ds.addSample([1, 0] * 3, [0])
 
+cfg = RbmGibbsTrainerConfig()
+cfg.maxIter = 3
+
 rbm = buildRbm(6, 1)
-trainer = RbmGibbsTrainer(rbm, ds)
+trainer = RbmGibbsTrainer(rbm, ds, cfg)
 print rbm.params
 trainer.train()
 print rbm.params
