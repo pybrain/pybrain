670655f54c168130e0b921ca24b0ce01ffdf7a0b
Tom Schaul <tom@idsia.ch>
2009-04-28 16:51:04 +0000

    consitency fix - first part of some larger changes to the evolution framework

--- pybrain/rl/learners/blackboxoptimizers/evolution/evolution.py
+++ pybrain/rl/learners/blackboxoptimizers/evolution/evolution.py
@@ -15,9 +15,10 @@ class Evolution(BlackBoxOptimizer):
     
     # evolution generally tries to maximize fitness, not minimize a function
     minimize = False
+    online = False
     
-    def __init__(self, f, **args):
-        BlackBoxOptimizer.__init__(self, f, **args)
+    def __init__(self, f, x0, **args):
+        BlackBoxOptimizer.__init__(self, f, x0, **args)
         assert self.minimize == False
         
         # current population
@@ -27,26 +28,25 @@ class Evolution(BlackBoxOptimizer):
         # for analysis purposes, store all kinds of stuff
         self.allgenerations = []
         
-        self.bestfitness = -1e100
-        self.bestx = None
+        self.bestEvaluation = -1e100
+        self.bestEvaluable = self.x0
         
     def stoppingCriterion(self):
-        return self.bestfitness >= self.stopPrecision
+        return self.bestEvaluation >= self.desiredEvaluation
         
-    def optimize(self):    
+    def _batchLearn(self, maxSteps):    
         """ the main loop """
         self.initPopulation()
         self.generation = 0
         while not self.stoppingCriterion():
             if self.generation > self.maxgenerations:
                 break
-            if len(self.tfun.vallist) > self.maxEvals:
+            if self.steps > maxSteps:
                 break
             self.oneGeneration()
             self.generation += 1
-            print 'Gen:', self.generation, 'fit:', self.bestfitness
-        return self.bestx    
-    
+            print 'Gen:', self.generation, 'fit:', self.bestEvaluation
+        
     def initPopulation(self):
         """ initialize the population """
         abstractMethod()
@@ -56,13 +56,13 @@ class Evolution(BlackBoxOptimizer):
         # evaluate fitness
         self.fitnesses = []
         for indiv in self.currentpop:
-            self.fitnesses.append(self.targetfun(indiv))
+            self.fitnesses.append(self.evaluator(indiv))
+            self.steps += 1
         
         # determine the best values
         best = argmax(array(self.fitnesses))
-        self.bestfitness = self.fitnesses[best]
-        self.bestx = self.currentpop[best]
-        
+        self.bestEvaluable = self.currentpop[best]
+        self.bestEvaluation = self.fitnesses[best]
         self.allgenerations.append((self.currentpop, self.fitnesses))
         
         self.produceOffspring()
