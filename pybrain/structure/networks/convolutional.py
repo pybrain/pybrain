ff76591fa156c443d7f9e8ae9591928f65abae09
Tom Schaul <tom@idsia.ch>
2009-05-19 23:35:10 +0000

    Added simple convolutional networks

---
+++ pybrain/structure/networks/convolutional.py
@@ -0,0 +1,83 @@
+from pybrain.structure.modules.linearlayer import LinearLayer
+from pybrain.structure.modules.tanhlayer import TanhLayer
+from pybrain.structure.moduleslice import ModuleSlice
+from pybrain.structure.networks.feedforward import FeedForwardNetwork
+from pybrain.structure.connections.shared import MotherConnection, SharedFullConnection
+from pybrain.rl.tasks.capturegame.capturetask import CaptureGameTask
+from pybrain.structure.modules.sigmoidlayer import SigmoidLayer
+
+__author__ = 'Tom Schaul, tom@idsia.ch'
+
+# TODO: code up a more general version
+# TODO: use modulemash.viewonflatlayer()
+
+
+class SimpleConvolutionalNetwork(FeedForwardNetwork):
+    """ A network with a specific form of weight-sharing, on a single 2D layer, 
+    convoluting neighboring inputs (within a square). """
+    
+    def __init__(self, inputdim, insize, convSize, numFeatureMaps, **args):
+        FeedForwardNetwork.__init__(self, **args)
+        inlayer = LinearLayer(inputdim*insize*insize)
+        self.addInputModule(inlayer)
+        self._buildStructure(inputdim, insize, inlayer, convSize, numFeatureMaps)
+        self.sortModules()
+        
+        
+    def _buildStructure(self, inputdim, insize, inlayer, convSize, numFeatureMaps):
+        #build layers        
+        outdim = insize - convSize + 1
+        hlayer = TanhLayer(outdim*outdim*numFeatureMaps, name = 'h')
+        self.addModule(hlayer)
+        
+        outlayer = SigmoidLayer(outdim*outdim, name = 'out')
+        self.addOutputModule(outlayer)
+        
+        # build shared weights
+        convConns = []
+        for i in range(convSize):
+            convConns.append(MotherConnection(convSize*numFeatureMaps*inputdim, name = 'conv'+str(i)))
+        outConn = MotherConnection(numFeatureMaps)
+        
+        # establish the connections.
+        for i in range(outdim):
+            for j in range(outdim):
+                offset = i * outdim + j
+                outmod = ModuleSlice(hlayer, inSliceFrom = offset*numFeatureMaps, inSliceTo = (offset+1)*numFeatureMaps,
+                                     outSliceFrom = offset*numFeatureMaps, outSliceTo = (offset+1)*numFeatureMaps)
+                self.addConnection(SharedFullConnection(outConn, outmod, outlayer, outSliceFrom = offset, outSliceTo = offset+1))
+                
+                for k, mc in enumerate(convConns):
+                    offset = insize * (i+k) + j
+                    inmod = ModuleSlice(inlayer, outSliceFrom = offset*inputdim, outSliceTo = offset*inputdim+convSize*inputdim)
+                    self.addConnection(SharedFullConnection(mc, inmod, outmod))
+            
+        
+if __name__ == '__main__':
+    from scipy import array, ravel
+        
+    N = ConvolutionalBoardNetwork(4, 3, 5)
+    input = [[[0,0],[0,0],[0,0],[0,0]],
+             [[0,0],[0,0],[0,0],[1,1]],
+             [[0,0],[1,1],[0,0],[0,1]],
+             [[0,0],[1,0],[1,1],[0,1]],
+             ]
+    res = N.activate(ravel(array(input)))
+    res = res.reshape(4,4)
+    print N['pad'].inputbuffer[0].reshape(6,6,2)[:,:,0]
+    print res
+    
+    t = CaptureGameTask(4)
+    print t(N)
+    
+    if False:        
+        N = SimpleConvolutionalNetwork(4, 2, 5)
+        print N
+        res = N.activate(ravel(array(input)))
+        res = res.reshape(3,3)
+        print res
+                
+            
+                
+                
+                
---
+++ pybrain/structure/networks/custom/convboard.py
@@ -0,0 +1,59 @@
+from pybrain.structure.modules.linearlayer import LinearLayer
+from pybrain.structure.moduleslice import ModuleSlice
+from pybrain.structure.connections.identity import IdentityConnection
+from pybrain.structure.networks.feedforward import FeedForwardNetwork
+from pybrain.structure.connections.shared import MotherConnection, SharedFullConnection
+from pybrain.structure.modules.biasunit import BiasUnit
+from pybrain.utilities import crossproduct
+from pybrain.structure.networks.convolutional import SimpleConvolutionalNetwork
+
+__author__ = 'Tom Schaul, tom@idsia.ch'
+
+
+class ConvolutionalBoardNetwork(SimpleConvolutionalNetwork):
+    """ A type of convolutional network, designed for handling game boards.
+    It pads the borders with a uniform bias input to allow one output per board position.
+    """
+    
+    def __init__(self, boardSize, convSize, numFeatureMaps, **args):
+        inputdim = 2
+        FeedForwardNetwork.__init__(self, **args)
+        inlayer = LinearLayer(inputdim*boardSize*boardSize, name = 'in')
+        self.addInputModule(inlayer)
+        
+        # we need some treatment of the border too - thus we pad the direct board input.
+        x = convSize/2
+        insize = boardSize+2*x
+        if convSize % 2 == 0: 
+            insize -= 1            
+        paddedlayer = LinearLayer(inputdim*insize*insize, name = 'pad')
+        self.addModule(paddedlayer)
+        
+        # we connect a bias to the padded-parts (with shared but trainable weights).
+        bias = BiasUnit()
+        self.addModule(bias)
+        biasConn = MotherConnection(inputdim)
+        
+        paddable = []
+        if convSize % 2 == 0: 
+            xs = range(x)+range(insize-x+1, insize)
+        else:
+            xs = range(x)+range(insize-x, insize)
+        paddable.extend(crossproduct([range(insize), xs]))
+        paddable.extend(crossproduct([xs, range(x, boardSize+x)]))
+        
+        for (i, j) in paddable:
+            self.addConnection(SharedFullConnection(biasConn, bias, paddedlayer, 
+                                                    outSliceFrom = (i*insize+j)*inputdim, 
+                                                    outSliceTo = (i*insize+j+1)*inputdim))
+                
+        for i in range(boardSize):
+            inmod = ModuleSlice(inlayer, outSliceFrom = i*boardSize*inputdim, 
+                                outSliceTo = (i+1)*boardSize*inputdim)
+            outmod = ModuleSlice(paddedlayer, inSliceFrom = ((i+x)*insize+x)*inputdim, 
+                                 inSliceTo = ((i+x)*insize+x+boardSize)*inputdim)
+            self.addConnection(IdentityConnection(inmod, outmod))
+            
+        self._buildStructure(inputdim, insize, paddedlayer, convSize, numFeatureMaps)
+        self.sortModules()
+                        
