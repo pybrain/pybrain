9a58593e0dc319c4572d2169a71f5285521a1d0a
Thomas Rueckstiess <ruecksti@in.tum.de>
2009-07-02 14:29:52 +0200

    added FullButSelfConnection.
    This type connects every element from the first module's output buffer to the second module's input buffer in a matrix multiplicative manner, EXCEPT the corresponding elements with the same index of each buffer (the diagonal of the parameter matrix is 0).
    
    Asserts that in and out dimensions are equal.

--- pybrain/structure/connections/__init__.py
+++ pybrain/structure/connections/__init__.py
@@ -2,3 +2,4 @@ from full import FullConnection
 from identity import IdentityConnection
 from shared import SharedFullConnection, MotherConnection, SharedConnection
 from linear import LinearConnection
+from fullnotself import FullNotSelfConnection
---
+++ pybrain/structure/connections/fullnotself.py
@@ -0,0 +1,28 @@
+__author__ = 'Thomas Rueckstiess, ruecksti@in.tum.de'
+
+from scipy import random, reshape, dot, outer, eye
+from pybrain.structure.connections import FullConnection
+from pybrain.structure.parametercontainer import ParameterContainer
+
+class FullNotSelfConnection(FullConnection):
+    """Connection which connects every element from the first module's 
+    output buffer to the second module's input buffer in a matrix multiplicative
+    manner, EXCEPT the corresponding elements with the same index of each buffer 
+    (the diagonal of the parameter matrix is 0). Asserts that in and out dimensions 
+    are equal. """
+    
+    def __init__(self, *args, **kwargs):
+        FullConnection.__init__(self, *args, **kwargs)
+        assert self.indim == self.outdim, \
+            "Indim (%i) does not equal outdim (%i)" % (
+            self.indim, self.outdim)
+    
+    def _forwardImplementation(self, inbuf, outbuf):
+       p = reshape(self.params, (self.outdim, self.indim)) * (1-eye(self.outdim))
+       outbuf += dot(p, inbuf)
+
+    def _backwardImplementation(self, outerr, inerr, inbuf):
+       p = reshape(self.params, (self.outdim, self.indim)) * (1-eye(self.outdim))
+       inerr += dot(p.T, outerr)
+       ds = self.derivs
+       ds += outer(inbuf, outerr).T.flatten()   
