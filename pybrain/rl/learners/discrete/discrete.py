d72637b6a81a3b4f6e4bc381401c847a8b0d9ff2
Thomas Rueckstiess <ruecksti@in.tum.de>
2009-09-21 15:44:38 +0200

    more comments.

--- pybrain/rl/learners/discrete/discrete.py
+++ pybrain/rl/learners/discrete/discrete.py
@@ -12,24 +12,28 @@ class DiscreteLearner(Learner):
     _explorer = None
 
     def __init__(self):
-        # create default explorer and hand it the module
+        """ create a default explorer for discrete learning tasks. """
         self.explorer = EpsilonGreedyExplorer()
 
     def _setModule(self, module):
+        """ set module and tell explorer about the module. """
         self.explorer.module = module
         self._module = module
 
     def _getModule(self):
+        """ return the module. """
         return self._module
         
     module = property(_getModule, _setModule)
 
     def _setExplorer(self, explorer):
+        """ set explorer and tell it the module, if already available. """
         self._explorer = explorer
         if self.module:
             self._explorer.module = self.module
 
     def _getExplorer(self):
+        """ return the explorer. """
         return self._explorer
         
     explorer = property(_getExplorer, _setExplorer)
