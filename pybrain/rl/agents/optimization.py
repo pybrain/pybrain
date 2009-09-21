fd33d26cf11a636c961237ab0a3c3d1448357d03
Thomas Rueckstiess <ruecksti@in.tum.de>
2009-09-21 15:40:58 +0200

    make OptimizationAgent derive from Agent to be consistent with the RL framework.

--- pybrain/rl/agents/optimization.py
+++ pybrain/rl/agents/optimization.py
@@ -1,9 +1,10 @@
 __author__ = 'Tom Schaul, tom@idsia.ch'
 
+from pybrain.rl.agents.agent import Agent
 
-class OptimizationAgent(object):
+class OptimizationAgent(Agent):
     """ A simple wrapper to allow optimizers to conform to the RL interface.
-    Works only in conjunction with EpisodicExperiment.
+        Works only in conjunction with EpisodicExperiment.
     """
     def __init__(self, module, learner):
         self.module = module
