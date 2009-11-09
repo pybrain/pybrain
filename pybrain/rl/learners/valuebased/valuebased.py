48e4b01674431e777b703f9df927953d487c7006
Thomas Rueckstiess <ruecksti@in.tum.de>
2009-11-09 16:51:08 +0100

    removed OntogeneticLearner from import list and added EpisodicLearner.

--- pybrain/rl/learners/valuebased/valuebased.py
+++ pybrain/rl/learners/valuebased/valuebased.py
@@ -1,6 +1,6 @@
 __author__ = 'Thomas Rueckstiess, ruecksti@in.tum.de'
 
-from pybrain.rl.learners.learner import ExploringLearner, DataSetLearner, OntogeneticLearner
+from pybrain.rl.learners.learner import ExploringLearner, DataSetLearner, EpisodicLearner
 from pybrain.rl.explorers.discrete.egreedy import EpsilonGreedyExplorer
 
 
