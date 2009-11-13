c1380a5a4f718bfee45ff0a61b46d944ba2c0edf
Frank Sehnke <sehnke@atknoll144.informatik.uni-muenchen.de>
2009-11-13 16:41:00 +0100

    added more examples

---
+++ examples/rl/environments/cartpole/cart_cma.py
@@ -0,0 +1,39 @@
+#########################################################################
+# Reinforcement Learning with PGPE on the CartPoleEnvironment 
+#
+# Requirements: pylab (for plotting only). If not available, comment the
+# 9 lines total marked as "for plotting"
+# Author: Frank Sehnke, sehnke@in.tum.de
+#########################################################################
+
+from pybrain.tools.example_tools import ExTools
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.rl.environments.cartpole import CartPoleEnvironment, BalanceTask
+from pybrain.rl.agents import OptimizationAgent
+from pybrain.optimization import CMAES
+from pybrain.rl.experiments import EpisodicExperiment
+
+batch=2
+prnts=100
+epis=4000/batch/prnts
+numbExp=10
+et = ExTools(batch, prnts)
+
+for runs in range(numbExp):
+    env = CartPoleEnvironment()    
+    # create task
+    task = BalanceTask(env, 200, desiredValue=None)
+    # create controller network
+    net = buildNetwork(4, 1, bias=False)
+    # create agent with controller and learner (and its options)
+    agent = OptimizationAgent(net, CMAES(storeAllEvaluations = True))
+    et.agent = agent
+    experiment = EpisodicExperiment(task, agent)
+    
+    for updates in range(epis):
+        for i in range(prnts):
+            experiment.doEpisodes(batch)
+        #print "Epsilon   : ", agent.learner.sigma
+        et.printResults((agent.learner._allEvaluations)[-50:-1], runs, updates)
+    et.addExps()
+et.showExps()
---
+++ examples/rl/environments/cartpole/cart_reinf.py
@@ -0,0 +1,39 @@
+#########################################################################
+# Reinforcement Learning with PGPE on the CartPoleEnvironment 
+#
+# Requirements: pylab (for plotting only). If not available, comment the
+# 9 lines total marked as "for plotting"
+# Author: Frank Sehnke, sehnke@in.tum.de
+#########################################################################
+
+from pybrain.tools.example_tools import ExTools
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.rl.environments.cartpole import CartPoleEnvironment, BalanceTask
+from pybrain.rl.agents import LearningAgent
+from pybrain.rl.learners import Reinforce
+from pybrain.rl.experiments import EpisodicExperiment
+
+batch=2
+prnts=100
+epis=4000/batch/prnts
+numbExp=10
+et = ExTools(batch, prnts, kind = "learner")
+
+for runs in range(numbExp):
+    env = CartPoleEnvironment()    
+    # create task
+    task = BalanceTask(env, 200, desiredValue=None)
+    # create controller network
+    net = buildNetwork(4, 1, bias=False)
+    # create agent with controller and learner (and its options)
+    agent = LearningAgent(net, Reinforce())
+    et.agent = agent
+    experiment = EpisodicExperiment(task, agent)
+    
+    for updates in range(epis):
+        for i in range(prnts):
+            experiment.doEpisodes(batch)
+        state, action, reward = agent.learner.dataset.getSequence(agent.learner.dataset.getNumSequences()-1)
+        et.printResults(reward.sum(), runs, updates)
+    et.addExps()
+et.showExps()
