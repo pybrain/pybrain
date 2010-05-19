531a7c934e78e49236ddc41b701752671a9215d9
Michele Orrù <maker.py@gmail.com>
2010-05-19 17:03:49 +0200

    reverting unexpected behaviour in removing trailing spaces

--- docs/code2tut.py
+++ docs/code2tut.py
@@ -3,20 +3,20 @@
 
 Synopsis:
     code2tut.py basename
-
+    
 Output:
     Will convert  tutorials/basename.py  into  sphinx/basename.txt
-
+    
 Conventions:
 1. All textual comments must be enclosed in triple quotation marks.
 2. First line of file is ignored, second line of file shall contain title in "",
    the following lines starting with # are ignored.
-3. Lines following paragraph-level markup (e.g. .. seealso::) must be indented.
+3. Lines following paragraph-level markup (e.g. .. seealso::) must be indented. 
    Paragraph ends with a blank line.
 4. If the code after a comment starts with a higher indentation level, you have
-   to manually edit the resulting file, e.g. by inserting "   ..." at the
+   to manually edit the resulting file, e.g. by inserting "   ..." at the 
    beginning of these sections.
-
+   
 See tutorials/fnn.py for example.
 """
 
@@ -31,7 +31,7 @@ f_in = file(os.path.join("tutorials",sys.argv[1])+".py")
 f_out = file(os.path.join("sphinx",sys.argv[1])+".txt", "w+")
 
 # write the header
-f_out.write(".. _"+sys.argv[1]+":\n\n")
+f_out.write(".. _"+sys.argv[1]+":\n\n")    
 f_in.readline()                                  #  ######################
 line = f_in.readline()
 line= line.split('"')[1]             #  # PyBrain Tutorial "Classification ..."
@@ -52,13 +52,13 @@ for line in f_in:
             continue
     elif begin:
         begin = False
-
+        
     if '"""' in line:
         for i in range(line.count('"""')):
             comment = 1 - comment
         if line.count('"""')==2:
             linecomment = True
-
+            
         line = line.replace('"""','')
         if comment==0:
             line += '::'
@@ -67,7 +67,7 @@ for line in f_in:
 
     elif comment==0 and line!='\n':
         line = "  "+line
-
+        
     if line.startswith('..'):
         inblock = True
     elif line=="\n":
@@ -75,14 +75,14 @@ for line in f_in:
 
     if (comment or linecomment) and not inblock:
         line = line.strip()+"\n"
-
+    
     if line.endswith("::"):
         line +='\n\n'
     elif line.endswith("::\n"):
         line +='\n'
-
+        
     f_out.write(line)
-
+    
 
 f_in.close()
 f_out.close()
--- docs/tutorials/blackboxoptimization.py
+++ docs/tutorials/blackboxoptimization.py
@@ -1,41 +1,41 @@
 ############################################################################
 # PyBrain Tutorial "Black Box Optimization"
-#
+# 
 # Author: Tom Schaul, tom@idsia.ch
 ############################################################################
 
 __author__ = 'Tom Schaul, tom@idsia.ch'
 
-""" A script that attempts to illustrate a large variety of
-use-cases for different kinds of black-box learning algorithms.
+""" A script that attempts to illustrate a large variety of 
+use-cases for different kinds of black-box learning algorithms. 
 """
 
 from pybrain.structure.networks.network import Network
 from pybrain.optimization import * #@UnusedWildImport
 
-""" The problem we would like to solve can be anything that
-has something like a fitness function.
-The following switches between two different examples.
+""" The problem we would like to solve can be anything that 
+has something like a fitness function. 
+The following switches between two different examples. 
 
-The variable 'theparams' contains the trainable
+The variable 'theparams' contains the trainable 
 parameters that affect the fitness. """
 
 if False:
     """ Simple function optimization:
-    here the parameters are learned directly. """
+    here the parameters are learned directly. """    
     from scipy import randn
     from pybrain.rl.environments.functions import SphereFunction
     thetask = SphereFunction(3)
     theparams = randn(3)
-
+    
 else:
     """ Simple pole-balancing task:
-    here we learn the weights of a neural network controller."""
+    here we learn the weights of a neural network controller."""   
     from pybrain.tools.shortcuts import buildNetwork
     from pybrain.rl.environments.cartpole.balancetask import BalanceTask
     thetask = BalanceTask()
-    theparams = buildNetwork(thetask.outdim, thetask.indim, bias=False)
-
+    theparams = buildNetwork(thetask.outdim, thetask.indim, bias=False)    
+    
 print 'Subsequently, we attempt to solve the following task:'
 print thetask.__class__.__name__
 
@@ -47,7 +47,7 @@ if isinstance(theparams, Network):
     for m in theparams.modules:
         print m.indim, ',',
     print ')\n'
-
+        
 """ We allow every algorithm a limited number of evaluations. """
 
 maxEvals = 1000
@@ -56,7 +56,7 @@ maxEvals = 1000
 
 print 'fmin', NelderMead(thetask, theparams, maxEvaluations=maxEvals).learn()
 
-""" The same, using some other algorithms
+""" The same, using some other algorithms 
 (note that the syntax for invoking them is always the same) """
 
 print 'CMA', CMAES(thetask, theparams, maxEvaluations=maxEvals).learn()
@@ -66,7 +66,7 @@ print 'Finite Differences', FiniteDifferences(thetask, theparams, maxEvaluations
 print 'SPSA', SimpleSPSA(thetask, theparams, maxEvaluations=maxEvals).learn()
 print 'PGPE', PGPE(thetask, theparams, maxEvaluations=maxEvals).learn()
 
-""" Evolutionary methods fall in the Learner framework as well.
+""" Evolutionary methods fall in the Learner framework as well. 
 All the following are examples."""
 
 print 'HillClimber', HillClimber(thetask, theparams, maxEvaluations=maxEvals).learn()
@@ -76,8 +76,8 @@ print 'ES 50+50', ES(thetask, theparams, maxEvaluations=maxEvals).learn()
 """ We can change some default parameters, e.g."""
 print 'ES 5+5', ES(thetask, theparams, mu=5, lambada=5, maxEvaluations=maxEvals).learn()
 
-""" Memetic algorithms are a kind of meta-algorithm, doing topology mutations
-on the top-level, and using other algorithms internally
+""" Memetic algorithms are a kind of meta-algorithm, doing topology mutations 
+on the top-level, and using other algorithms internally 
 as a kind of local search (default there: hill-climbing)."""
 
 print 'Memetic Climber', MemeticSearch(thetask, theparams, maxEvaluations=maxEvals).learn()
@@ -96,7 +96,7 @@ print 'Inner Memetic Climber', InnerMemeticSearch(thetask, theparams, maxEvaluat
 print 'Inner Memetic CMA', InnerMemeticSearch(thetask, theparams, maxEvaluations=maxEvals,
                                               localSearch=CMAES).learn()
 
-""" Inverse memetic algorithms do local search on topology mutations,
+""" Inverse memetic algorithms do local search on topology mutations, 
 and weight changes in the top-level search. """
 
 print 'Inverse Memetic Climber', InverseMemeticSearch(thetask, theparams, maxEvaluations=maxEvals).learn()
