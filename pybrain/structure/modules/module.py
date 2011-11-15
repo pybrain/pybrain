76e3a0fd6ac8cefb50efdcc64bbcad9808506efd
Tom Schaul <schaul@gmail.com>
2011-11-15 18:53:58 -0500

    Bugfix for _resetBuffers()

--- pybrain/structure/modules/module.py
+++ pybrain/structure/modules/module.py
@@ -54,6 +54,8 @@ class Module(Named):
         """Reset buffers to a length (in time dimension) of 1."""
         for buffername, dim in self.bufferlist:
             setattr(self, buffername, zeros((length, dim)))
+        if length==1:
+            self.offset = 0
 
     def _growBuffers(self):
         """Double the size of the modules buffers in its first dimension and
@@ -92,7 +94,7 @@ class Module(Named):
         if items == 0:
             return
         self.offset += items
-        for buffername, l  in self.bufferlist:
+        for buffername, _  in self.bufferlist:
             buf = getattr(self, buffername)
             assert abs(items) <= len(buf), "Cannot shift further than length of buffer."
             fill = zeros((abs(items), len(buf[0])))
