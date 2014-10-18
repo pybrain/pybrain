3fe1a8e08df297f1256257252e34e471c6b22068
Ian Fischer <ian@apportable.com>
2014-10-17 19:41:34 -0700

    Remove EOL whitespace.

--- pybrain/rl/environments/functions/function.py
+++ pybrain/rl/environments/functions/function.py
@@ -22,7 +22,7 @@ class FunctionEnvironment(Environment, FitnessEvaluator):
     # what would be the desired performance? by default: something close to zero
     desiredValue = 1e-10
     toBeMinimized = True
-    
+
     # does the function already include a penalization term, to keep search near the origin?
     penalized = False
 
