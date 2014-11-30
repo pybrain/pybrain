3b1fbeba7ba4fd33b1c0fcce6bfcc07d258d293e
Shane Bretz <bretzs@rpi.edu>
2014-11-30 00:30:04 -0500

    Update fnn.py

--- docs/tutorials/fnn.py
+++ docs/tutorials/fnn.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 ############################################################################
 # PyBrain Tutorial "Classification with Feed-Forward Neural Networks"
 #
