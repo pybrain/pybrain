9ba58a8e544f2b1095d4575347d363281a2cf220
asamsonov <samsonov68rus@gmail.com>
2015-04-25 12:40:15 +0300

    add freq_matrix to kohonen map improve color maps add show_map function for plotting kohonen maps

--- pybrain/tools/plotting/colormaps.py
+++ pybrain/tools/plotting/colormaps.py
@@ -2,27 +2,40 @@ __author__ = 'Tom Schaul, tom@idsia.ch'
 
 from pylab import figure, savefig, imshow, axes, axis, cm, show
 from scipy import array, amin, amax
+from matplotlib.pyplot import colorbar
+from numpy.linalg import norm
+
+from pybrain.structure.modules import KohonenMap
 
 
 class ColorMap:
-    def __init__(self, mat, cmap=None, pixelspervalue=20, minvalue=None, maxvalue=None):
+    def __init__(self, mat, cmap=None, pixelspervalue=20, normalize=False, interpolation='nearest',
+                 bar=True, minvalue=None, maxvalue=None, name=None):
         """ Make a colormap image of a matrix
 
         :key mat: the matrix to be used for the colormap.
+        :key normalize:
         """
+        if normalize:
+            mat_norm = norm(mat)
+            mat = array(map(lambda x: map(lambda y: y / mat_norm, x), mat))
         if minvalue == None:
             minvalue = amin(mat)
         if maxvalue == None:
             maxvalue = amax(mat)
         if not cmap:
             cmap = cm.hot
-
         figsize = (array(mat.shape) / 100. * pixelspervalue)[::-1]
         self.fig = figure(figsize=figsize)
-        axes([0, 0, 1, 1]) # Make the plot occupy the whole canvas
+        axes([0, 0, 1, 1])  # Make the plot occupy the whole canvas
         axis('off')
         self.fig.set_size_inches(figsize)
-        imshow(mat, cmap=cmap, clim=(minvalue, maxvalue), interpolation='nearest')
+        self.im = imshow(mat, cmap=cmap, clim=(minvalue, maxvalue), interpolation=interpolation)
+        if bar:
+            colorbar()
+        if name is not None:
+            self.fig.canvas.set_window_title(name)
+
 
     def show(self):
         """ have the image popup """
@@ -32,3 +45,10 @@ class ColorMap:
         """ save colormap to file"""
         savefig(filename, fig=self.fig, facecolor='black', edgecolor='black')
 
+
+def show_map(som, interpolation='nearest'):
+    assert isinstance(som, KohonenMap)
+    keys = som.keys if som.keys is not None else [i for i in xrange(som.nInput)]
+    for i in xrange(som.nInput):
+        ColorMap(som.neurons[:, :, i], bar=True, name=keys[i], interpolation=interpolation)
+    # show()
